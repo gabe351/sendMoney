b41971ec84976e7739001529f3bd52289129184e
Gaberu <gabrielrosadn@gmail.com>
2018-03-15 21:00:09 -0300

    initial commit
    Signed-off-by: Gaberu <gabrielrosadn@gmail.com>

---
+++ SendMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SendMoney
+//
+//  Created by Gabriel Rosa on 15/03/18.
+//  Copyright © 2018 Gabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
